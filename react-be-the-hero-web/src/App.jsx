1eeec53e75cee3bcebcc873c587b3ffbdf66e48f
Felipe Chan <felipe_s.c1@hotmail.com>
2020-04-05 23:34:41 -0300

    iniciando layout web

---
+++ react-be-the-hero-web/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import Logon from './pages/Logon';
+import './global.css';
+import Routes from './routes';
+
+function App() {
+
+  return (
+    <div>
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
